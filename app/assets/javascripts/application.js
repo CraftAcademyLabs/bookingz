0b56c68ddb029749552bf4db996e5a14890c7a7c
tochman <thomas@craftacademy.se>
2016-08-06 09:57:16 +0200

    makes shift to foundation 6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
+
+
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def callout(key)
+    case key
+      when 'success' then
+        'success'
+      when 'warning' then
+        'warning'
+      when 'alert' then
+        'alert'
+      else
+        'primary'
+    end
+  end
 end
