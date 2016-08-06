6c58fc28989d038949a5e47da858a89a4587d8ab
tochman <thomas@craftacademy.se>
2016-08-06 23:00:54 +0200

    updates seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,19 @@ description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident.'
 
 Resource.all.each { |r| r.destroy }
-Resource.create(designation: 'Lilla konferanssalen 1', description: description, capacity: 8, schedule: IceCube::Schedule.new(Time.now, duration: 10.hours))
-Resource.create(designation: 'Lilla konferanssalen 2', description: description, capacity: 8, schedule: IceCube::Schedule.new(Time.now, duration: 10.hours))
-Resource.create(designation: 'Stora konferanssalen 1', description: description, capacity: 20, schedule: IceCube::Schedule.new(Time.now, duration: 10.hours))
-Resource.create(designation: 'Stora konferanssalen 2', description: description, capacity: 20, schedule: IceCube::Schedule.new(Time.now, duration: 10.hours))
-Resource.create(designation: 'Stora kongresshallen', description: description, capacity: 200, schedule: IceCube::Schedule.new(Time.now, duration: 10.hours))
-Resource.create(designation: 'Lilla kongresshallen', description: description, capacity: 100, schedule: IceCube::Schedule.new(Time.now, duration: 10.hours))
+Resource.create(designation: 'Lilla konferanssalen 1', description: description, capacity: 8,
+                schedule: IceCube::Schedule.new(Date.today, duration: 1.day))
+Resource.create(designation: 'Lilla konferanssalen 2', description: description, capacity: 8,
+                schedule: IceCube::Schedule.new(Date.today, duration: 1.day))
+Resource.create(designation: 'Stora konferanssalen 1', description: description, capacity: 20,
+                schedule: IceCube::Schedule.new(Date.today, duration: 1.day))
+Resource.create(designation: 'Stora konferanssalen 2', description: description, capacity: 20,
+                schedule: IceCube::Schedule.new(Date.today, duration: 1.day))
+Resource.create(designation: 'Stora kongresshallen', description: description, capacity: 200,
+                schedule: IceCube::Schedule.new(Date.today, duration: 1.day))
+Resource.create(designation: 'Lilla kongresshallen', description: description, capacity: 100,
+                schedule: IceCube::Schedule.new(Date.today, duration: 1.day))
 
+Resource.all.each do |resource|
+  resource.schedule.add_recurrence_rule IceCube::Rule.daily
+end
