9a90ecdc638f47e2555e44d4431a86dafa0431e7
tochman <thomas@craftacademy.se>
2016-08-15 15:16:16 +0200

    refactors the Ping spec

--- spec/requests/api/api_ping_spec.rb
+++ spec/requests/api/api_ping_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-describe 'API namespece' do
-
-  it 'GET /api/ping' do
-    get '/api/ping'
-    expect(response.status).to eq 200
-    expect(response_json['message']).to eq 'Pong'
+describe 'API namespace' do
+  describe 'Ping endpoint' do
+    it 'GET /api/ping' do
+      get '/api/ping'
+      expect(response.status).to eq 200
+      expect(response_json['message']).to eq 'Pong'
+    end
   end
 end
