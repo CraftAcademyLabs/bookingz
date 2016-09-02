710cec71480b2be1e59efd6348d9d3c6580f66f6
MikaelFeher <mikael.feher@gmail.com>
2016-09-02 23:51:56 +0200

    Adds method in the pages controller for future pages Adds a page ca_labs just to test controller functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,19 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    render 'pages/instructions'
+    render page_to_render
+  end
+
+  private
+
+  def page_to_render
+    case params[:id]
+    when 'instructions'
+      'instructions'
+    when 'ca_labs'
+      'ca_labs'
+    else
+      raise 'You have not defined the page yet'
+    end
   end
 end
