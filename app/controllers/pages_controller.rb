f54ddacd6d60091b55b08cfdc23add829a194bf0
MikaelFeher <mikael.feher@gmail.com>
2016-09-03 11:46:38 +0200

    Removes the show method in the pages controller to let high_voltage gem do its magic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,20 +2,16 @@ class PagesController < ApplicationController
   include HighVoltage::StaticPage
   before_action :authenticate_user!
 
-  def show
-    render page_to_render
-  end
+  :page_to_render
 
   private
 
   def page_to_render
     case params[:id]
-    when 'instructions'
-      'instructions'
     when 'ca_labs'
       'ca_labs'
-    else
-      raise 'You have not defined the page yet'
+    when 'instructions'
+      'instructions'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   post 'create_booking', controller: :dashboard, action: :create_booking
   root controller: :dashboard, action: :index
-  resources :pages, only: [:show]
+  get "/pages/*id" => 'pages#show', as: :page, format: false
 
   namespace :api do
     get 'ping', controller: :api, action: :ping
