8001bb0a4a581ac18d03343bc2f92d23e6ccb033
MikaelFeher <mikael.feher@gmail.com>
2016-09-03 15:17:34 +0200

    Cleans up commented code in pages controller Fixes a broken link in the header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,4 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
   before_action :authenticate_user!
-
-  #:page_to_render
-
-  private
-
-  # def page_to_render
-  #   case params[:id]
-  #   when 'ca_labs'
-  #     'ca_labs'
-  #   when 'instructions'
-  #     'instructions'
-  #   end
-  # end
 end
