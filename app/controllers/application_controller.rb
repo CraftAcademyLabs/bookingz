1e40ada2c2d7c92a333eccd2b2e4b37cc238dcbe
lollypop27 <lara@adfactory.co.za>
2016-09-04 15:34:47 +0200

    add swedish translation to yml add translation tags to html files add set local method to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  # def set_locale
+  #   logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+  #   I18n.locale = extract_locale_from_accept_language_header
+  #   logger.debug "* Locale set to '#{I18n.locale}'"
+  # end
+  #
+  # private
+  #   def extract_locale_from_accept_language_header
+  #     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  #   end
+  #
+  #   def default_url_option(options ={})
+  #     {locale: I18n.locale}
+  #   end
 end
