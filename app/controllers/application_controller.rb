d527e86d284e046f5a3104279a6d8e5d98750262
lollypop27 <lara@adfactory.co.za>
2016-09-05 18:07:36 +0200

    refactoring I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-        locale :
-        I18n.default_locale
+   I18n.locale = params[:locale] || I18n.default_locale
   end
 end
