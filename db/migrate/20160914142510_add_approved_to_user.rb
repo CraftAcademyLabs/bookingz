b855916e86b93c4bf3fe8e6781d4efb85821b963
Lucia De Mojà <luciademoja@gmail.com>
2016-09-15 10:12:52 +0200

    migrates db and adds methods to add and remove a user

--- db/migrate/20160914142510_add_approved_to_user.rb
+++ db/migrate/20160914142510_add_approved_to_user.rb
@@ -1,5 +1,11 @@
 class AddApprovedToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :approved, :boolean
+  def self.up
+    add_column :users, :approved, :boolean, :default => false, :null => false
+    add_index  :users, :approved
+  end
+
+  def self.down
+    remove_index  :users, :approved
+    remove_column :users, :approved
   end
 end
