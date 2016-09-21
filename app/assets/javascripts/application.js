a47b9d435067c318d8b07ee71f14487a9e190f86
tochman <thomas@craftacademy.se>
2016-09-21 08:39:48 +0200

    resolved conflicts in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -15,9 +14,74 @@
 //= require jquery_ujs
 //= require foundation
 //= require foundation-datetimepicker
-//= require header_footer
 // require turbolinks
 //= require_tree .
 
+$(document).ready(function () {
+    dateOnPageLoad();
+    var date;
+    if (location.search.substr(1).length > 0) {
+        date = location.search.substr(1).split("=")[1];
+        $('#date').html(date);
+    } else {
+        date = currentDate();
+    }
+    queryApi(date);
+
+    //$('.picker').fdatetimepicker({
+    //    language: 'en',
+    //    pickTime: true,
+    //    closeButton: true,
+    //    startView: 0,
+    //    format: 'hh:ii'
+    //});
+
+    //setupDateTimePicker();
+
+    var now = new Date();
+    //var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+    //var checkout = $('#booking_time_end').fdatetimepicker({
+    //    onRender: function (date) {
+    //        return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+    //    }
+    //}).on('changeDate', function (ev) {
+    //    checkout.hide();
+    //}).data('datepicker');
+    var checkout = $('#booking_time_end').fdatetimepicker({
+        startDate: now,
+        pickTime: 0,
+        startView: 0,
+        format: 'hh:ii',
+        maxView: 0,
+        onRender: function(time){
+            return time.valueOf() < checkin.date.valueOf() ? 'disabled' : '';
+        }
+    }).on('changeDate', function () {
+        checkout.hide();
+    }).data('datetimepicker');
+    var checkin = $('#booking_time_start').fdatetimepicker({
+        startDate: now,
+        startView: 0,
+        format: 'hh:ii',
+        maxView: 0,
+        onRender: function (time) {
+            return time.valueOf() < now.valueOf() ? 'disabled' : '';
+        }
+    }).on('changeDate', function (ev) {
+        //if (ev.date.valueOf() > checkout.date.valueOf()) {
+        //    var newDate = new Date(ev.date);
+        //    newDate.setDate(newDate.getDate() + 1);
+        //    checkout.update(newDate);
+        //}
+        checkin.hide();
+        $('#booking_time_end').focus();
+    }).data('datetimepicker');
 
 
+});
+
+
+function setupDateTimePicker() {
+
+
+}
