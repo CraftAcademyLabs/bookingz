5547be3347e299138b65228a58acf238e848fe9a
tochman <thomas@craftacademy.se>
2016-09-23 10:15:52 +0200

    Still WIP. Progress on timeslots. issues with disabeling times

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,67 +18,7 @@
 //= require_tree .
 
 $(document).ready(function () {
-    dateOnPageLoad();
-    var date;
-    if (location.search.substr(1).length > 0) {
-        date = location.search.substr(1).split("=")[1];
-        $('#date').html(date);
-    } else {
-        date = currentDate();
-    }
-    queryApi(date);
-
-    //$('.picker').fdatetimepicker({
-    //    language: 'en',
-    //    pickTime: true,
-    //    closeButton: true,
-    //    startView: 0,
-    //    format: 'hh:ii'
-    //});
-
-    //setupDateTimePicker();
-
-
-
 
 });
 
 
-function setupDateTimePicker() {
-
-    var now = new Date();
-
-    var checkout = $('#booking_time_end').fdatetimepicker({
-        initalDate: this.valueOf(),
-        startDate: now,
-        pickTime: 0,
-        startView: 0,
-        format: 'hh:ii',
-        maxView: 0,
-        onRender: function(time){
-            debugger;
-            return time.valueOf() < this.valueOf() ? 'disabled' : '';
-        }
-    }).on('show', function(){
-        console.log('showing');
-    }).on('changeDate', function () {
-        checkout.hide();
-    }).data('datetimepicker');
-    var checkin = $('#booking_time_start').fdatetimepicker({
-        startDate: now,
-        startView: 0,
-        format: 'hh:ii',
-        maxView: 0,
-        onRender: function (time) {
-            return time.valueOf() < now.valueOf() ? 'disabled' : '';
-        }
-    }).on('changeDate', function (ev) {
-        if (ev.date.valueOf() > checkout.date.valueOf()) {
-            var newDate = new Date(ev.timeStamp);
-            newDate.setMinutes(newDate.getMinutes() + 30);
-            checkout.update(newDate);
-        }
-        checkin.hide();
-        $('#booking_time_end').focus();
-    }).data('datetimepicker');
-}
