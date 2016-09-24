5e88c2e8f28440089ad4691c80c8a393227830e8
tochman <thomas@craftacademy.se>
2016-09-24 19:50:11 +0200

    restart phantmjs around all '@javascript' scenarios

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,6 @@
-After do
-  Capybara.reset_sessions!
+After '@javascript' do
+  Capybara.send('session_pool').each do |_, session|
+    next unless session.driver.is_a?(Capybara::Poltergeist::Driver)
+    session.driver.restart
+  end
 end
