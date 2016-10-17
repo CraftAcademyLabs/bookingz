3c585595a9e13941945177fb5f4dcdb48424ee5d
Raoul DIFFOUO <diraulo@gmail.com>
2016-10-17 22:23:03 +0200

    extract some common dashboard functions into dashboard.js

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,51 @@
+function queryApi(date) {
+  $.ajax({
+    dataType: "json",
+    url: '/api/resources?date=' + date,
+    success: function (response) {
+      loadCurrentBookings(response);
+      addEvents();
+    }
+  });
+}
+
+function updateApi(date) {
+  $.ajax({
+    dataType: "json",
+    url: '/api/resources?date=' + date,
+    success: function (response) {
+      updateCurrentBookings(response);
+      addEvents();
+    }
+  });
+}
+
+function currentDate() {
+  var date = setDate().toJSON().slice(0, 10);
+  return date;
+}
+
+function setDate() {
+  return new Date();
+}
+
+function getDisplayedDate() {
+  var date = $('#date').text();
+  return date;
+}
+
+function weekOf(date) {
+  return ['Week of',
+    date.getThisWeeksMonday().sv_format(), '-',
+    date.getThisWeeksSunday().sv_format()
+  ].join(' ');
+}
+
+function today() {
+  return new Date(new Date().toJSON().slice(0, 10) + " 00:00");
+}
+
+function getInfo(obj) {
+  var message = (obj.info.client != null) ? setSlotMessage(obj) : obj.info.time;
+  return message;
+}
