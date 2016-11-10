4f57ee213cf4d96eed95c8a98ae35a0d983851e6
Raoul DIFFOUO <diraulo@gmail.com>
2016-11-10 21:46:14 +0200

    switch back to coveralls 😏

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require 'simplecov'
-SimpleCov.command_name 'RSpec'
+require 'coveralls'
+Coveralls.wear_merged!('rails')
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
