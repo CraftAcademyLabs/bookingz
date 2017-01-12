8f5a63ae99ab661dbfb9c965814d283999e414f0
sigu <sigumagwa@gmail.com>
2017-01-12 13:34:30 +0300

    add direction to resource and validate presence

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,7 +3,7 @@ class Resource < ApplicationRecord
                    capacity_type: :closed,
                    bookable_across_occurrences: true
 
-  validates_presence_of :designation, :uuid, :facility
+  validates_presence_of :designation, :uuid, :facility, :direction
   validates_uniqueness_of :uuid
 
   belongs_to :facility
